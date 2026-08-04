15bb281cc76e2b871109300c06d813d68815943f
Fatih Mar <mfatihmar@gmail.com>
2026-08-04 12:24:13 +0100

    server http mux and serve public dir

--- server/main.go
+++ server/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello, world")
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("public")))
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "pong")
+	})
+	err := http.ListenAndServeTLS(":8443", "cert.pem", "key.pem", mux)
+	if err != nil {
+		slog.Error("cannot listen", "err", err)
+		os.Exit(1)
+	}
 }
